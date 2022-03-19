de037fd1f39f6a288d5611de74e1f6e0956c5522
Karol <gilkarol304@gmail.com>
2022-03-19 14:26:49 +0100

    refactored cod

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,24 +1,23 @@
-
-import { Strategy, ExtractJwt } from 'passport-jwt'
+import { Strategy, ExtractJwt } from 'passport-jwt';
 import { User } from '../entity/User';
 import database from '../util/database';
 
 export default (passport: any) => {
-    const options = {
-        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-        secretOrKey: process.env.JWT_SECRET_TOKEN,
-    }
-    passport.use(
-        new Strategy(options, async (jwt_payload: any, done: any) => {
-            try {
-                const userRepository = (await database).getRepository(User)
-                const user = await userRepository.findOne({ id: jwt_payload.userId })
-                if (user) return done(null, user)
-                return done(null, false)
-            } catch (err) {
-                return done(err, false)
-            }
-        })
-    )
-    
-}
+	const options = {
+		jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+		secretOrKey: process.env.JWT_SECRET_TOKEN,
+	};
+	passport.use(
+		new Strategy(options, async (jwt_payload: any, done: any) => {
+			try {
+				const user = (await database)
+					.getRepository(User)
+					.findOne({ id: jwt_payload.userId });
+				if (user) return done(null, user);
+				return done(null, false);
+			} catch (err) {
+				return done(err, false);
+			}
+		})
+	);
+};
