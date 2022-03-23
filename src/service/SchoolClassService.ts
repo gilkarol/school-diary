46e63cb90a47dd08ab5c543dde91aa4522c63793
Karol <gilkarol304@gmail.com>
2022-03-23 19:20:28 +0100

    added functions to service

--- src/service/SchoolClassService.ts
+++ src/service/SchoolClassService.ts
@@ -2,6 +2,8 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 import { SchoolClass, SchoolClassDto } from '../entity/SchoolClass';
 import { Service } from 'typedi';
+import { HttpError } from '../util/classes';
+import { Profile } from '../entity/Profile';
 
 @Service()
 export class SchoolClassService {
@@ -10,8 +12,49 @@ export class SchoolClassService {
 		protected readonly schoolClassRepository: Repository<SchoolClass>
 	) {}
 
+	async findByShortName(shortName: string): Promise<SchoolClass> {
+		const schoolClass = await this.schoolClassRepository.findOne({
+			shortName: shortName,
+		});
+		if (!schoolClass) {
+			throw new HttpError(404, `Class ${shortName} does not exist!`);
+		}
+		return schoolClass;
+	}
+
+	async findAll(): Promise<SchoolClass[]> {
+		return await this.schoolClassRepository.find();
+	}
+
 	async create(schoolClassDto: SchoolClassDto): Promise<SchoolClass> {
 		const schoolClass = this.schoolClassRepository.create(schoolClassDto);
+		return await this.schoolClassRepository.save(schoolClass);
+	}
+
+	async addTutorToClass(
+		shortName: string,
+		teacherProfile: Profile
+	): Promise<SchoolClass> {
+		const schoolClass = await this.findByShortName(shortName);
+		schoolClass.tutor = teacherProfile;
+		return await this.schoolClassRepository.save(schoolClass);
+	}
+
+	async removeTutorFromClass(
+		shortName: string,
+		teacherProfile: Profile
+	): Promise<SchoolClass> {
+		const schoolClass = await this.findByShortName(shortName);
+		schoolClass.tutor = undefined;
+		return await this.schoolClassRepository.save(schoolClass);
+	}
+
+	async addStudentToClass(
+		shortName: string,
+		studentProfile: Profile
+	): Promise<SchoolClass> {
+		const schoolClass = await this.findByShortName(shortName);
+		schoolClass.students?.push(studentProfile);
 		return await this.schoolClassRepository.save(schoolClass);
 	}
 }
