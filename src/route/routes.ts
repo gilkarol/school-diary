2e962eabf75cd64dbbabb4b3f144dc507fefbde7
Karol <gilkarol304@gmail.com>
2022-03-30 14:43:17 +0200

    changes in routes

--- src/route/routes.ts
+++ src/route/routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import authRoutes from './auth';
+import errorRoutes from './error';
+import adminRoutes from './admin/routes';
 
 const router = Router();
 
+router.use('/admin', adminRoutes);
+
 router.use('/auth', authRoutes);
+
+router.use(errorRoutes);
 
 export default router;
