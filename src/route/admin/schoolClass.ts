34041807f14e33c4ed122cda3f9254bee3e5d012
Karol <gilkarol304@gmail.com>
2022-03-30 19:53:12 +0200

    added removing and adding tutor to the class

--- src/route/admin/schoolClass.ts
+++ src/route/admin/schoolClass.ts
@@ -5,6 +5,7 @@ import { checkRole } from '../../middleware/checkRole';
 import { isAuth } from '../../middleware/isAuth';
 import { SchoolClassService } from '../../service/SchoolClassService';
 import { StudentProfileService } from '../../service/StudentProfileService';
+import { TeacherProfileService } from '../../service/TeacherProfileService';
 
 const router = Router();
 
@@ -78,6 +79,57 @@ router.post(
 				message: 'Student has been successfully removed from the class!',
 				school_class: schoolClass,
 			});
+		} catch (err) {
+			return next(err);
+		}
+	}
+);
+
+// ADD TUTOR TO CLASS
+router.post(
+	'/:classShortName/addTutor/:teacherId',
+	isAuth,
+	checkRole('admin'),
+	async (req, res, next) => {
+		const schoolClassService = Container.get(SchoolClassService);
+		const teacherProfileService = Container.get(TeacherProfileService);
+		const { classShortName, teacherId } = req.params;
+		try {
+			const teacherProfile = await teacherProfileService.findById(teacherId);
+			const schoolClass = await schoolClassService.addTutorToClass(
+				classShortName,
+				teacherProfile
+			);
+			res
+				.status(200)
+				.json({
+					message: 'Tutor added to the class!',
+					school_class: schoolClass,
+				});
+		} catch (err) {
+			return next(err);
+		}
+	}
+);
+
+// REMOVE TUTOR FROM CLASS
+router.post(
+	'/:classShortName/addTutor/:teacherId',
+	isAuth,
+	checkRole('admin'),
+	async (req, res, next) => {
+		const schoolClassService = Container.get(SchoolClassService);
+		const { classShortName } = req.params;
+		try {
+			const schoolClass = await schoolClassService.removeTutorFromClass(
+				classShortName
+			);
+			res
+				.status(200)
+				.json({
+					message: 'Tutor removed from the class!',
+					school_class: schoolClass,
+				});
 		} catch (err) {
 			return next(err);
 		}
