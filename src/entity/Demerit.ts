3c6e828604ce01349ffe3e0cc3be6559aa9e0b61
Karol <gilkarol304@gmail.com>
2022-03-31 14:07:32 +0200

    added Demerit

--- src/entity/Demerit.ts
+++ src/entity/Demerit.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, ManyToOne, PrimaryColumn } from 'typeorm';
+import {
+	Column,
+	CreateDateColumn,
+	Entity,
+	ManyToOne,
+	PrimaryColumn,
+	UpdateDateColumn,
+} from 'typeorm';
 import uuid from '../util/uuid';
 import { Profile } from './Profile';
 
@@ -12,9 +19,25 @@ export class Demerit {
 	@PrimaryColumn({ default: uuid })
 	id?: string;
 
+	@Column()
+	type?: string;
+
 	@ManyToOne(() => Profile, (profile) => profile.demerits)
 	profile?: Profile;
 
 	@Column()
 	description?: string;
+
+	@CreateDateColumn({
+		type: 'timestamp',
+		default: () => 'CURRENT_TIMESTAMP(6)',
+	})
+	created_at?: Date;
+
+	@UpdateDateColumn({
+		type: 'timestamp',
+		default: () => 'CURRENT_TIMESTAMP(6)',
+		onUpdate: 'CURRENT_TIMESTAMP(6)',
+	})
+	updated_at?: Date;
 }
